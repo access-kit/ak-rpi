15c347a150b0246b5c1715148505176b1d911ca0
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2025-01-22 11:44:23 -0500

    create offline mode

--- ak_rpi/main.py
+++ ak_rpi/main.py
@@ -8,6 +8,29 @@ from ak_rpi.errors import BaseRegistrationError
 logger = logging.getLogger(__name__)
 
 
+def offline_mode():
+    """Run the player in offline mode.
+
+    In this mode, the player will play the first available audio file in the media dir and loop indefinitely.
+    """
+    from ak_rpi.audio import AudioPlayer
+    from ak_rpi.player import MediaDir
+    from ak_rpi.utils import scan_for_exts
+
+    paths = scan_for_exts(MediaDir, ["mp3", "wav"], recursive=True)
+    if len(paths) == 0:
+        logger.exception("No audio files found.")
+        return
+    fpath = paths[0].as_posix()
+    logger.info(f"Found audio file: {fpath}")
+    audio = AudioPlayer.Load(fpath)
+    while True:
+        audio.stop()
+        audio.play()
+        while audio.remaining_ms > 0:
+            pass
+
+
 def main():
     """Main function."""
     try:
@@ -16,7 +39,7 @@ def main():
         logger.exception(
             "Failed to acquire player settings; will start in offline mode.", exc_info=e
         )
-        # TODO: implement offline mode
+        offline_mode()
     else:
         player.setup()
         player.run()
