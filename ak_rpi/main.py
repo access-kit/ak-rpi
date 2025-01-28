21e28386a1cc813858a1aba43d42045cb8b7b945
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2025-01-28 16:57:19 -0500

    create offline mode (#4)
    * create offline mode
    
    * allow any execution error to result in offline mode
    
    * force offline mode
    
    * add debug message
    
    * add more debug messages
    
    * add more debug messages
    
    * change structure of main
    
    * remove some debug messages
    
    * force exception in main mode
    
    * use nested try/except to handle runtime fallback

--- ak_rpi/main.py
+++ ak_rpi/main.py
@@ -8,20 +8,52 @@ from ak_rpi.errors import BaseRegistrationError
 logger = logging.getLogger(__name__)
 
 
+def offline_mode():
+    """Run the player in offline mode.
+
+    In this mode, the player will play the first available audio file in the media dir and loop indefinitely.
+    """
+    from ak_rpi.audio import AudioPlayer
+    from ak_rpi.player import MediaDir
+    from ak_rpi.utils import scan_for_exts
+
+    logger.info(f"Starting offline mode, searching for audio files in {MediaDir}...")
+    paths = scan_for_exts(MediaDir, ["mp3", "wav"], recursive=True)
+    logger.info(f"Found {len(paths)} audio files.")
+    if len(paths) == 0:
+        logger.exception("No audio files found.")
+        return
+    fpath = paths[0].as_posix()
+    logger.info(f"Found audio file: {fpath}")
+    audio = AudioPlayer.Load(fpath)
+    while True:
+        audio.stop()
+        audio.play()
+
+        while audio.remaining_ms > 0:
+            pass
+
+
 def main():
     """Main function."""
+    logging.basicConfig(level=logging.INFO)
+
     try:
-        player = RegistrationData.FromConfig()
-    except BaseRegistrationError as e:
-        logger.exception(
-            "Failed to acquire player settings; will start in offline mode.", exc_info=e
-        )
-        # TODO: implement offline mode
-    else:
-        player.setup()
-        player.run()
+        try:
+            player = RegistrationData.FromConfig()
+        except BaseRegistrationError as e:
+            logger.exception(
+                "Failed to acquire player settings; will start in offline mode.",
+                exc_info=e,
+            )
+            offline_mode()
+        else:
+            player.setup()
+            player.run()
+    except Exception as e:
+        logger.exception("Failure in main loop, switching to offline mode.", exc_info=e)
+        offline_mode()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
